5f69c8ace4f4d3b9843e133140fd7bab6173191d
Danillo Santos <dnashy@hotmail.com>
2023-11-06 13:57:46 -0300

    feat: add the process function

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,20 @@
+import sys
+from .file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    file_list = txt_importer(path_file)
+    for i in range(len(instance.queue)):
+        if instance.queue[i]["nome_do_arquivo"] == path_file:
+            return None
+
+    new_dict = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(file_list),
+        "linhas_do_arquivo": file_list,
+    }
+    instance.enqueue(new_dict)
+    sys.stdout.write(str(new_dict))
 
 
 def remove(instance):
