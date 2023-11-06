fceba9029074191aa771aaa819858d643273b71e
Danillo Santos <dnashy@hotmail.com>
2023-11-06 15:01:19 -0300

    test: add tests to the priority_queue class

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -1,5 +1,37 @@
-# from ting_file_management.priority_queue import PriorityQueue
+from ting_file_management.priority_queue import PriorityQueue
+import pytest
 
 
 def test_basic_priority_queueing():
-    """Aqui irá sua implementação"""
+    priority_queue = PriorityQueue()
+    data1 = {"nome_do_arquivo": "file1.txt", "qtd_linhas": 3}
+    data2 = {"nome_do_arquivo": "file2.txt", "qtd_linhas": 6}
+
+    # Test enqueue and dequeue operations
+    priority_queue.enqueue(data1)
+    priority_queue.enqueue(data2)
+
+    assert priority_queue.dequeue() == data1
+    assert priority_queue.dequeue() == data2
+
+    # Test the length of the queue
+    assert len(priority_queue) == 0
+
+    priority_queue.enqueue(data1)
+    assert len(priority_queue) == 1
+
+    priority_queue.enqueue(data2)
+    assert len(priority_queue) == 2
+
+    priority_queue.dequeue()
+    assert len(priority_queue) == 1
+
+    # Test the search method
+    priority_queue.enqueue(data1)
+    priority_queue.enqueue(data2)
+
+    assert priority_queue.search(0) == data1
+    assert priority_queue.search(1) == data2
+
+    # Test searching for an index out of range
+    pytest.raises(IndexError, priority_queue.search, 3)
