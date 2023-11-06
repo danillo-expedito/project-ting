f86c9e36fb91c67cd0f3993c5381ba4eca7950d0
Danillo Santos <dnashy@hotmail.com>
2023-11-06 12:57:09 -0300

    feat: add the file management function

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,14 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith(".txt"):
+        sys.stderr.write("Formato inválido")
+        return None
+
+    try:
+        with open(path_file, "r") as file:
+            return file.read().splitlines()
+    except FileNotFoundError:
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
+        return None
